c607a976b345df9cc233b103086ecab77e4f7207
JosePK0 <157852544+JosePK0@users.noreply.github.com>
2024-09-15 18:18:56 -0600

    API VS2

--- Services/Features/Mangas/MangaService.cs
+++ Services/Features/Mangas/MangaService.cs
@@ -1,40 +1,51 @@
 using mangas.Domain.Entities;
+using mangas.Infrastructure.Repositories;
+
 namespace mangas.Services.Freatures.manga;
 
-public class  MangaService
-{
-    private readonly List<Manga> _mangas = new();
+    public class MangaService
+    {
+        private readonly MangaRepository _mangaRepository;
 
-    public IEnumerable<Manga> GetAll()
-    {
-        return _mangas;
-    }
-
-    public Manga GetById(int id)
-    {
-        return _mangas.FirstOrDefault(manga => manga.Id == id);
-    }
-
-    public void Add(Manga manga)
-    {
-        _mangas.Add(manga);
-    }
-    public void Update(Manga mangaToUpdate)
-    {
-        var manga = GetById(mangaToUpdate.Id);
-        if (manga != null)
+        public MangaService(MangaRepository mangaRepository)
         {
-            _mangas.Remove(manga);
-            _mangas.Add(mangaToUpdate);
+            this._mangaRepository = mangaRepository;
+        }
+
+        public IEnumerable<Manga> GetAll()
+        {
+            return _mangaRepository.GetAll();
+        }
+
+        public Manga GetById(int id)
+        {
+            return _mangaRepository.GetById(id);
+        }
+
+        public void Add(Manga manga)
+        {
+            _mangaRepository.Add(manga);
+        }
+
+        public void Update(Manga mangaToUpdate)
+        {
+            var manga = GetById(mangaToUpdate.Id);
+
+            if (manga.Id > 0)
+            {
+                _mangaRepository.Update(mangaToUpdate);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var manga = GetById(id);
+
+            if (manga.Id > 0)
+            {
+                _mangaRepository.Delete(id);
+            }
         }
     }
-     public void Delete(int id)
-     {
-        var manga = GetById(id);
-        if (manga != null)
-        {
-            _mangas.Remove(manga);
-        }
-     }
 
-}
+
--- obj/Debug/net7.0/mangas.AssemblyInfo.cs
+++ obj/Debug/net7.0/mangas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mangas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aafc5013d02588e524e1b249e8d94e87be2daf64")]
 [assembly: System.Reflection.AssemblyProductAttribute("mangas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mangas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
